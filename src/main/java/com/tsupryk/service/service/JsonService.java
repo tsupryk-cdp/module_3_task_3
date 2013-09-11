7df99ffb8558bc824d0be94de78b9954c46b0a1f
tsupryk <tsupryk@gmail.com>
2013-09-11 18:44:44 +0300

    added json parsing to datasets

--- src/main/java/com/tsupryk/service/service/JsonService.java
+++ src/main/java/com/tsupryk/service/service/JsonService.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -37,14 +39,51 @@ public class JsonService implements IJsonService {
             jacksonMarshaller.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
 
             map = jacksonMarshaller.readValue(data, HashMap.class);
+
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        return map;
+        return performToCitiesData(map);
     }
 
-    private String performToCitiesData(String data) {
+    private Map<String, List<List<String>>> performToCitiesData(Map<String, Object> data) {
 
-        return null;
+        Map<String, List<List<String>>> datasets = new HashMap<>();
+
+
+        Map<String, Object> all = (Map<String, Object>) data.get("EPAM");
+
+        for (Object yearName : all.keySet()) {
+            String yearNameString = (String) yearName;
+            String year = yearNameString.replace("Year", "");
+
+            Map<String, Object> towns = (Map<String, Object>) all.get(yearNameString);
+
+
+
+            for (String townName : towns.keySet()) {
+                if (datasets.get(townName) == null){
+                    datasets.put(townName, new ArrayList<List<String>>());
+                }
+
+                List<Integer> townValuesList = (List<Integer>) towns.get(townName);
+
+                for (int i = 0; i<townValuesList.size(); i++) {
+                    List<String> point = new ArrayList<>();
+                    Integer y = new Integer(year);
+                    point.add(String.valueOf(y + (0.25 * i)));
+                    point.add(townValuesList.get(i).toString());
+                    datasets.get(townName).add(point);
+                }
+
+            }
+
+            towns.getClass();
+
+
+        }
+
+
+        return datasets;
     }
 }
